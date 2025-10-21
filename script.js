e93884f1b7b6d7559b1a96fb5569690c3302e392
ToonyTime.BotAcc <deledaounblocked@gmail.com>
2025-10-21 09:35:54 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const navToggle = document.getElementById('nav-toggle');
+    const mainNav = document.querySelector('.main-nav');
+
+    if (navToggle && mainNav) {
+        navToggle.addEventListener('click', () => {
+            mainNav.classList.toggle('is-open');
+        });
+    }
+});
 
